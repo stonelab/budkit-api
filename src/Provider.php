a421391e54514e2506cf60d33bada0c15fb34cee
Livingstone Fultang <livingstonefultang@gmail.com>
2015-09-09 11:27:01 +0100

    Initial commit

---
+++ src/Provider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Api;
+
+use Budkit\Application\Support\Service;
+use Budkit\Dependency\Container;
+
+class Provider implements Service {
+
+    protected $application;
+
+    public function __construct(Container $application) {
+        $this->application = $application;
+    }
+
+
+    public static function  getPackageDir(){
+        return __DIR__."/";
+    }
+
+    public function onRegister() {
+
+        \Route::attach("/api", "App\\Api\\Controller\\Protocol", function($route){
+            $route->setTokens(array(
+                'id'		=> '\d+',
+                'format'	=> '(\.[^/]+)?'
+            ));
+            //subroutes
+            $route->add('{format}','index');
+            $route->add('{/id}{format}', "view");
+            $route->add('{/id}/edit{format}', "edit");
+
+        });
+    }
+
+    public function definition() {
+        return ["app.register" => "onRegister"];
+    }
+}
